1f889f7285038a07f5230624211825363bcf557e
ajmandourah <ajmandourah@gmail.com>
2024-10-11 16:10:10 +0300

    Add '/config' as a config path

--- config/config.go
+++ config/config.go
@@ -68,6 +68,7 @@ func New() repository.Config {
 func (cfg *Configuration) LoadConfig() {
 	viper.SetConfigName("config")     // name of config file (without extension)
 	viper.SetConfigType("yaml")       // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("/config")          // optionally look for config in the working directory
 	viper.AddConfigPath(".")          // optionally look for config in the working directory
 	viper.SetTypeByDefaultValue(true) // Allows []string to be parsed from Env Vars
 
