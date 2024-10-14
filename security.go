1ed25f3327979b4bdf901b20aecea5a70a058f8c
ajmandourah <ajmandourah@gmail.com>
2024-10-14 18:57:37 +0300

    .

--- security.go
+++ security.go
@@ -37,6 +37,7 @@ func (s *TinShop) TinfoilMiddleware(next http.Handler) http.Handler {
 			next.ServeHTTP(w, r)
 			return
 		}
+		log.Println(headers["Hauth"])
 
 		//Show Hauth for the specefied host
 		if r.RequestURI == "/hauth" && headers["Hauth"] != nil {
